0051ad276f71dc06432e7d2239a81e521c04f171
Missael Burciaga Hdz <missaelburciaga37@gmail.com>
2024-03-09 13:12:56 -0600

    Agregar archivos de proyecto.

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var persona = new Persona
+            {
+                Nombre="Missael Burciaga Hernandez",
+                Edad=23
+            };
+            return View(persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
